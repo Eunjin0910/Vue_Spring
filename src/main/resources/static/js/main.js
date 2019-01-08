daee086e4830d46334745dd71aa334781da59ece
Alvin <alvin@cntt.co.kr>
2019-01-08 17:29:49 +0900

    message 입력후 save 클릭시 insert 구현  - 입력후 input 박스 ""변경 구현 X  - LIST 바로바로 등록되게끔 완성
     3강 5:10까지 완성

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,23 +1,54 @@
+Vue.component('message-form',{
+    props : ['messages'],
+    data: function(){
+      return {
+          text: ''
+      }
+    },
+    template:'<div>' +
+                '<input type="text" placeholder="Write someting" v-model="text">' +
+                '<input type="button" value="SAVE" @click="save">' +
+             '</div>',
+    methods: {
+        save: function(){
+            var messages = this.messages;
+            var message = {text : this.text};
+
+            axios.post('/message',message).then(function(resultdata){
+               messages.push(resultdata.data);
+            });
+        }
+    }
+
+});
+
+
 Vue.component('message-row', {
     props : ['message'],
-    template : '<div><i>({{ message.id }})</i> {{ message.text }}</div>'
+    template : '<div>' +
+                    '<i>({{ message.id }})</i> {{ message.text }}' +
+               '</div>'
 });
 
 
 Vue.component('message-list',{
     props: ['messages'],
-    template: '<div><message-row v-for="message in messages" :key="message.id" :message="message" /></div>',
+    template: '<div>' +
+                    '<message-form :messages="messages" />' +
+                    '<message-row v-for="message in messages" :key="message.id" :message="message" />' +
+              '</div>',
     created: function() {
+        var messages = this.messages;
         axios.get('/message',{
         }).then(function(response){
-            console.log(response.data);
-            response.data.forEach(function (message){
-                console.log(message);
-                app.messages.push(message);
+            response.data.forEach(function (resultdata){
+                messages.push(resultdata);
             });
         });
     }
 });
+
+Vue.config.devtools = true;
 
 var app = new Vue({
    el : '#app',
@@ -26,3 +57,4 @@ var app = new Vue({
        messages : []
    }
 });
+
